87feed154338f1737540e8e6a99367c3b694532a
tylertucker202 <tylertucker202@gmail.com>
2021-03-31 15:51:47 -1000

    created generic aggregation functions

--- controller_helper.py
+++ controller_helper.py
@@ -5,25 +5,62 @@ def create_collection(dbName, collName, port=27017):
     client = pymongo.MongoClient(dbUrl)
     db = client[dbName]
     coll = db[collName]
-    return coll    
+    return coll
 
-def get_semesters(obsName, coll):
-    pipeline = [
-        {
-            "$match": {
-                "$and" : [{
-                    'obs_id' : {
-                        "$exists": "true", "$in": [ f"{obsName}"]
-                    }
-                }]
-            }
-        },
-        {
-        "$project": {
-            "semester": 1,
+def create_match(key, item):
+    if key == 'observers':
+        mItem = { 
+            f"signature.observers": {
+            "$exists": "true", "$in": [ f"{item}"]
+        }}
+    else:
+        mItem = {
+            f"signature.{key}": item
+        }
+    return mItem
+
+def create_signature_match(coll, qdict):
+    '''note matching flattens nested fields'''
+    matchItems = [ create_match(key, item) for key, item in qdict.items() ]        
+    return {
+        "$match": {
+            "$and": matchItems
+        }
+    }
+    
+def group_distinct_signature(key):
+    '''group by one field in aggreate pipeline'''
+    return {
+            "$group":{
+                "_id": { key: f"$signature.{key}" }
             }
         }
+
+def project_fields(*signatureKeys):
+    return {"$project": { f"{key}": 1 for key in signatureKeys } }
+
+def project_signature_fields(*signatureKeys):
+    return {"$project": { f"signature.{key}": 1 for key in signatureKeys } }
+    
+def get_distinct_semesters(obsName, coll):
+    match = create_signature_match(coll, {'observers': obsName})
+    group = group_distinct_signature('semester')
+    pipeline = [
+        match,
+        group
     ]
+    return coll.aggregate(pipeline)
+
+def get_semesters_by_pi(piName, coll):
+    match = create_signature_match(coll, {'pi': piName})
+    project = project_signature_fields('semester')
+    pipeline = [ match, project ]
+    return coll.aggregate(pipeline)
+
+def get_ob_by_semester(semid, coll):
+    match = create_signature_match(coll, {'semester': semid})
+    project = project_signature_fields('semester')
+    pipeline = [ match, project ]
     return coll.aggregate(pipeline)
 
 def find_by_pi(name, coll):
