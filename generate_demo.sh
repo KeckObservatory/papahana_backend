15fd3b374600835c913f6aada5a7429f3042a29a
tylertucker202 <tylertucker202@gmail.com>
2021-05-04 16:57:10 -1000

    trying to create with json

--- generate_demo.sh
+++ generate_demo.sh
@@ -1,4 +1,5 @@
 docker run --rm -v ${PWD}:/local swaggerapi/swagger-codegen-cli-v3 generate \
-    -i papahana_demo_2.yaml \
+    -i https://github.com/KeckObservatory/papahana_backend/blob/main/papahana_demo_2.yaml \
     -l python-flask \
-    -o /papahana_flask_server_demo
+    -o /papahana_flask_server_demo \
+    -v
