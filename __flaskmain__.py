683083cc1e40117406c163bac8d76dde3644e39a
tylertucker202 <tylertucker202@gmail.com>
2021-05-11 12:07:29 -1000

    hacky fix to undo autogen __main__.py overwrite

---
+++ __flaskmain__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from flask_cors import CORS
+import connexion
+from swagger_server import encoder
+
+
+def main():
+    app = connexion.App(__name__, specification_dir='./swagger/')
+    CORS(app.app)
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('swagger.yaml', arguments={'title': 'Papahana_demo'}, pythonic_params=True)
+    app.run(port=50000)
+
+
+if __name__ == '__main__':
+    main()
