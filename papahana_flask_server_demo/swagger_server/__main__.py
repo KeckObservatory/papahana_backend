213821d4a71dea1463480a111293a4a1f2471502
Josh Riley <joshjriley@gmail.com>
2021-05-12 10:35:01 -1000

    cors allows request from www3build

--- papahana_flask_server_demo/swagger_server/__main__.py
+++ papahana_flask_server_demo/swagger_server/__main__.py
@@ -5,7 +5,8 @@ from swagger_server import encoder
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
-    CORS(app.app)
+    CORS(app.app, resources={r"https://www3build.keck.hawaii.edu/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}})
+
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Papahana_demo'}, pythonic_params=True)
     app.run(port=50000)
