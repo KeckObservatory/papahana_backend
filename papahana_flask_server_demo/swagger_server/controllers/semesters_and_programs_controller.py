ed98a9cf152fe679c8665bbe6cf0012777df574b
Lucas Fuhrman <lfuhrman@mac-lfuhrman.hawaii.rr.com>
2021-05-17 15:45:04 -1000

    added route to program with obs_id and sem_id

--- papahana_flask_server_demo/swagger_server/controllers/semesters_and_programs_controller.py
+++ papahana_flask_server_demo/swagger_server/controllers/semesters_and_programs_controller.py
@@ -9,82 +9,6 @@ from swagger_server.controllers import controller_helper as utils
 from swagger_server import util
 
 
-#TODO rthis needs to be changed to observer instead of pi
-# def semester_programs_get(obs_id, semester=None):
-#     """retrieves all the programs associated with an observer
-#
-#     :param obs_id: observer id
-#     :type obs_id: int
-#     :param semester: semester
-#     :type semester: str
-#
-#     :rtype: List[Program]
-#     """
-#
-#     if connexion.request.is_json:
-#         obs_id = object.from_dict(connexion.request.get_json())
-#
-#     if not obs_id:
-#         return []
-#
-#     query = {"signature.pi_id": obs_id}
-#     if semester:
-#         query["signature.sem_id"] = {'$regex': f'{semester}_.*'}
-#
-#     ob_blocks = utils.get_by_query(query, 'obCollect')
-#
-#     prog_dict = {}
-#     for ob in ob_blocks:
-#         if ('_id' not in ob or 'signature' not in ob
-#                 or "sem_id" not in ob['signature']):
-#             continue
-#
-#         sem_id = ob['signature']['sem_id']
-#         block_id = str(ob['_id'])
-#         if sem_id in prog_dict:
-#             program = prog_dict[sem_id]
-#             ob_list = program.observation_blocks
-#             if block_id not in ob_list:
-#                 ob_list.append(block_id)
-#             program.observation_blocks = ob_list
-#             prog_dict[sem_id] = program
-#         else:
-#             parts = sem_id.split('_')
-#             if len(parts) == 2:
-#                 name = parts[1]
-#             else:
-#                 name = ""
-#             prog_dict[sem_id] = Program(name, sem_id, [block_id])
-#
-#     program_list = list(prog_dict.values())
-#
-#     return program_list
-
-#
-# def semester_programs_get(obs_id, semester=None):
-#     """
-#     retrieves all the programs associated with an observer
-#
-#     :param obs_id: observer id
-#     :type obs_id: int
-#     :param semester: semester
-#     :type semester: str
-#
-#     :rtype: List[Program]
-#     """
-#
-#     if connexion.request.is_json:
-#         obs_id = object.from_dict(connexion.request.get_json())
-#
-#     query = {"$or": [{"pi_id": obs_id}, {"observers_id": obs_id}]}
-#     if semester:
-#         query["sem_id"] = {'$regex': f'{semester}_.*'}
-#
-#     results = utils.get_by_query(query, 'prgCollect')
-#
-#     return utils.clean_objectid(results)
-
-
 def programs_get(obs_id):  # noqa: E501
     """retrieves all the programs associated with an observer.
 
@@ -102,14 +26,6 @@ def programs_get(obs_id):  # noqa: E501
 
     return sem_ids
 
-
-    # query = {"pi_id": obs_id}
-    #
-    # # if semester:
-    # #     query["sem_id"] = {'$regex': f'{semester}_.*'}
-    #
-    # results = utils.get_by_query(query, 'prgCollect')
-    # return utils.clean_objectid(results)
 
 def sem_id_proposal_get(sem_id, obs_id):  # noqa: E501
     """sem_id_proposal_get
